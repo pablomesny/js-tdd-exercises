764419f4a74316b6485cbfb9b8c75efeeb3efdcd
pablomesny <pablomesny@gmail.com>
2023-06-02 12:50:01 -0300

    exercise 1-08 done

--- I.pass-tests/08-get-average/get-average.test.js
+++ I.pass-tests/08-get-average/get-average.test.js
@@ -1,10 +1,45 @@
-var average = require("./get-average");
+const getAverage = require("./get-average");
 
-test("Average", function() {
-  var numbers = [4, "-", 8, 11, "hello", "57", 0, 2];
-  var expected = 5;
+describe('Given the getAverage function', () => {
+  test('Given the input [4, "-", 8, 11, "hello", "57", 0, 2] it should return 5 and keep the original one', () => {
+    const numbers = [4, "-", 8, 11, "hello", "57", 0, 2];
+    const unchanged = [4, "-", 8, 11, "hello", "57", 0, 2];
+    const expected = 5;
+  
+    const output = getAverage(numbers);
+  
+    expect(output).toEqual(expected);
+    expect(numbers).toEqual(unchanged);
+  });
 
-  var output = average(numbers);
+  test('Given the input ["test", "text", "fail"] show throw an error', () => {
+    const input = ['test', 'text', 'fail'];
+    expect( () => getAverage( input ) ).toThrow('invalid input');
+  })
 
-  expect(output).toEqual(expected);
-});
+  test('Given the input 200 should return 200 and keep the original one', () => {
+    const number = 200;
+    const unchanged = 200;
+    const expected = 200;
+
+    const output = getAverage( number );
+
+    expect(output).toEqual(expected);
+    expect(number).toEqual(unchanged);
+  })
+
+  test('Given the input "test" it should throw an error', () => {
+    const input = 'test';
+    expect( () => getAverage( input ) ).toThrow('invalid input');
+  })
+
+  test('Given the input undefined it should throw an error', () => {
+    const input = undefined;
+    expect( () => getAverage( input ) ).toThrow('invalid input');
+  })
+
+  test('Given the input null it should throw an error', () => {
+    const input = null;
+    expect( () => getAverage( input ) ).toThrow('invalid input');
+  })
+})
