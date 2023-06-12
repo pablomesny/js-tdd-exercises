19905d6c7b937ca8a6abed72019db9c3c4c096f9
pablomesny <pablomesny@gmail.com>
2023-06-12 10:58:19 -0300

    exercise 02-01 done

--- II.write-tests/01-greet-people/greet-people.js
+++ II.write-tests/01-greet-people/greet-people.js
@@ -1,9 +1,34 @@
-function greetPeople(people) {
-  var greeting = "Hello ";
+const greetPeople = (people) => {
 
-  people.forEach(function(person) {
-    greeting = greeting + person;
-  });
+  const isAString = typeof people === 'string';
+  const isAnArray = Array.isArray(people);
+  const isAnArrayOfStrings = isAnArray && people.every( person => typeof person === 'string' );
+
+  if( !isAString && !isAnArrayOfStrings ) {
+    throw new Error('invalid input');
+  }
+
+  if( !isAnArrayOfStrings ) {
+    return `Hello ${people}.`;
+  }
+
+  let greeting = "Hello";
+
+  if( isAnArrayOfStrings ) {
+    people.forEach( (person, index) => {
+  
+      const isFirstItem = index === 0;
+      const isMiddleItem = index >= 1 && index < people.length -1;
+  
+      if( isFirstItem ) {
+        greeting += ` ${person}`;
+      } else if( isMiddleItem ) {
+        greeting += `, ${person}`;
+      } else {
+        greeting += ` and ${person}.`;
+      }
+    });
+  }
 
   return greeting;
 }
--- II.write-tests/01-greet-people/greet-people.test.js
+++ II.write-tests/01-greet-people/greet-people.test.js
@@ -1,5 +1,42 @@
-test("print list of names prefixed with Hello", function() {
-  // Arrange
-  // Act
-  // Assert
+const greetPeople = require("./greet-people");
+
+describe('Given the greetPeople function', () => {
+  test('Given the array ["Irina", "Ashleigh", "Etza"] it should return "Hello Irina, Ashleigh and Etza." and keep the original array', () => {
+    // Arrange
+    const input = ['Irina', 'Ashleigh', 'Etza'];
+    const unchanged = ['Irina', 'Ashleigh', 'Etza'];
+    const expected = 'Hello Irina, Ashleigh and Etza.';
+
+    // Act
+    const output = greetPeople(input);
+
+    // Assert
+    expect(output).toEqual(expected);
+    expect(input).toEqual(unchanged);
+  });
+
+  test('Given the input "Juan" it should return "Hello Juan." and keep the original string', () => {
+    const input = 'Juan';
+    const unchanged = 'Juan';
+    const expected = 'Hello Juan.';
+
+    const output = greetPeople(input);
+
+    expect(output).toEqual(expected);
+    expect(input).toEqual(unchanged);
+  });
+
+  test('Given the input null it should throw an error', () => {
+    const input = null;
+    expect( () => greetPeople(input) ).toThrow('invalid input');
+  });
+
+  test('Given the input undefined it should throw an error', () => {
+    const input = undefined;
+    expect( () => greetPeople(input) ).toThrow('invalid input');
+  });
+
+  test('Given no input it should throw an error', () => {
+    expect( () => greetPeople() ).toThrow('invalid input');
+  });
 });
